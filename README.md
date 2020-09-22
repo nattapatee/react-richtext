280e3072039585d296ddb093ba3becb28d51950d
nattapatee <nattapat.ek@bcircle.co.th>
2020-09-22 17:29:22 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# React-richtext
+
+
+## Installation
+
+```bash
+  npm install @tinymce/tinymce-react
+  npm install deasync
+```
+
+## Usage
+
+```bash
+  npm run start
+```
+
+## Credit
+[Link](https://www.tiny.cloud/docs/integrations/react/)
---
+++ src/index.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react'
+import { Component } from 'react'
+import { render } from 'react-dom'
+// import './index.css'
+import { Editor } from '@tinymce/tinymce-react';
+
+class App extends Component {
+
+  handleEditorChange = (content, editor) => {
+    console.log('Content was updated:', content);
+  }
+
+  render() {
+    return (
+      <Editor
+      initialValue="<p>This is the initial content of the editor</p>"
+      init={{
+        height: 500,
+        menubar: false,
+        plugins: [
+          'advlist autolink lists link image charmap print preview anchor',
+          'searchreplace visualblocks code fullscreen',
+          'insertdatetime media table paste code help wordcount'
+        ],
+        toolbar:
+          'undo redo | formatselect | bold italic backcolor | \
+          alignleft aligncenter alignright alignjustify | \
+          bullist numlist outdent indent | removeformat | help'
+      }}
+      onEditorChange={this.handleEditorChange}
+    />
+    )
+  }
+}
+
+render(<App />, document.getElementById('root'))
